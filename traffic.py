74ddbe8d88b850e525cb0a13dca2e5eaee429c1a
Marcos Dominguez <mrds0690@gmail.com>
2020-09-13 12:15:38 -0300

    'define car and traffic class'

---
+++ traffic.py
@@ -0,0 +1,6 @@
+from vehicle import Car
+
+carTest=Car(60)
+print(carTest.speed)
+
+
---
+++ ventana.py
@@ -0,0 +1,7 @@
+from processing_py import *
+
+app=App(600,400) # create window: width, height
+app.background(100,100,100)
+app.redraw()
+
+app.exit()
