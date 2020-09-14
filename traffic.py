57d46c00e6f02a1798be04e1e73ee54ada09392d
Marcos Dominguez <mrds0690@gmail.com>
2020-09-14 15:56:23 -0300

    create traficc class and set cars initialization

--- traffic.py
+++ traffic.py
@@ -3,14 +3,25 @@ from street import Street
 import methods
 from land import Land
 import random
+import visual
+from visual import *
+
+
+class Traffic:
+    
+    def __init__(self,cars,width, height,streetQuantity):
+        self.__land=Land(width,height,streetQuantity)
+        self.__cars:list=[]
+        for x in range(streetQuantity):
+            print("street ",x,':')
+            print(" start: ",self.__land.streetList[x].begining)
+            print(" max speed limit: ",self.__land.streetList[x].maxSpeedLimit)
+        
+        for y in range(cars):
+            self.__cars.append((Vehicle(random.randint(0,len(self.__land.streetList)-1),self.__land)))
+            print('initial position: ',self.__cars[y].position)
 
 
 
-land1=Land(1000,1500,8)
-for x in range(8):
-    print(land1.streetList[x].begining)
+Traffic(5,1000,800,11)
 
-a=land1.streetList[x].maxSpeedLimit
-print(a)
-
-carTest=Vehicle(random.randint(0,len(land1.streetList)),land1)
